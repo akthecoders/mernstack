78f6f1d59b7f1190035cef3b1d0d0064d91a1be7
akthecoders <akthecoder@gmail.com>
2019-08-07 08:13:52 +0530

    Till registration

---
+++ routes/api/auth.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+
+// @route   GET api/auth
+// @desc    Test route
+// @access  Public
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    return res.json({ user });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
